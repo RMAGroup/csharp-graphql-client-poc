ea07066405e2afff2eafbd5f452eba1fc620d5fc
Paul Krisadaolarn <pauly.k@gmail.com>
2023-01-28 16:31:11 +0700

    initial

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# Strawberry Shake Graphql Client POC
+
+Investigate using a dotnet based graphql client.
+
+Currently the `skd-function` is based on javascript + graphql codegen + apollo client.
+
+The tooling and setup require time and effort to learn and setup.
+
+Based on: [Get started with Strawberry Shake in a Console application](https://chillicream.com/docs/strawberryshake/v12/get-started/console)
+
+
---
+++ src/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SKD.Operations;
+using StrawberryShake;
+
+Console.WriteLine("start");
+// Setup IConfiguration
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables()
+    .Build();
+
+// get configuration setting "AppTitle"
+var skdServerGraphqlUrl = configuration["SkdGraphqlServerUrl"] ?? "";
+
+Console.WriteLine($"skdServerGraphqlUrl: {skdServerGraphqlUrl}");
+
+var services = new ServiceCollection();
+services.AddSkdGqlClient(ExecutionStrategy.CacheAndNetwork)
+    .ConfigureHttpClient(c => c.BaseAddress = new Uri(skdServerGraphqlUrl));
+
+
+var serviceProvider = services.BuildServiceProvider();
+var client = serviceProvider.GetRequiredService<ISkdGqlClient>();
+
+var result = await client.GetComponents.ExecuteAsync();
+
+if (result.Data?.Components?.Nodes is not null)
+{
+    foreach (var component in result.Data.Components.Nodes)
+    {
+        Console.WriteLine(component.Name);
+    }
+}
+
+
